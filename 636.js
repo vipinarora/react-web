f5f36ce4a59a6620cb38b8cfd4945fa506f35cff
vipinarora <48980087+vipinarora@users.noreply.github.com>
2022-08-29 12:33:36 +0530

    commited new files

---
+++ 636.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhome=self.webpackChunkhome||[]).push([[636],{636:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u});var l=n(985),s=n.n(l);function u(){return s().createElement("div",{className:"p-5 bg-blue-500 text-white text-3xl font-bold"},"Only The Best Spinners")}}}]);
--- README.md
+++
@@ -1,2 +0,0 @@
-# react-web
-this is react website
